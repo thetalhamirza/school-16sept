8b3c8160f1b3b3dad403d13ea86b94f92823e806
thetalhamirza <talha9to5@gmail.com>
2023-09-23 02:31:04 +0500

    Used try-except

--- Q1.py
+++ Q1.py
@@ -1,7 +1,14 @@
 #Q1
-num = int(input("Enter a number: "))
 
-for i in range(1,11):
-    print(num, "x", i, "=",end=' ')
-    print(num*i)
+try:
+    num = int(input("Enter a number: "))
+    for i in range(1,11):
+        print(num, "x", i, "=",end=' ')
+        print(num*i)
+
+except ValueError:
+    print('plz enter number')
+
+
+
 
